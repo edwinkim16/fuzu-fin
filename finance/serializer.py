4aba3a2f450fb732014a29518652c0eb536f5657
edwinkim16 <edwin.kithinji@student.moringaschool.com>
2021-11-19 00:16:32 +0300

    serializing models

---
+++ finance/serializer.py
@@ -0,0 +1,21 @@
+from .models import *
+from rest_framework import serializers
+
+class ApproveSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Approve
+        fields=["id","name","created_at","updated_at","amount"]
+
+class SupportSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Support
+        fields=["id","message"]        
+
+class StaffSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Staff
+        fields=['__all__']  
+class PayrollSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Payroll
+        fields=["id","date","full_name","payment_type"]     
