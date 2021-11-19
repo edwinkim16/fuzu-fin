e86ff96fc7df3bedb2cba5585d6281a0d45652c8
edwinkim16 <edwin.kithinji@student.moringaschool.com>
2021-11-19 07:15:05 +0300

    debugging cors header

--- main/settings.py
+++ main/settings.py
@@ -82,6 +82,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'main.urls'
